fc72cf65debbdc821e2e136fd31bde6270d9df17
Steve Miner <steveminer@gmail.com>
2018-03-05 10:43:24 -0500

    init

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # pklbrain
-All rights reserved
+Nothing to see here.
+
+Copyright © 2018 Stephen E. Miner
+
+All rights reserved.
---
+++ project.clj
@@ -0,0 +1,5 @@
+(defproject com.velisco/pklbrain "0.1.0-SNAPSHOT"
+  :description "PKL Brain"
+  :url "https://github.com/miner/pklbrain"
+  :dependencies [[org.clojure/clojure "1.9.0"]])
+
