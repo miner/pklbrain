287b139620615661deee1a6b59272ef2c26eac94
Steve Miner <steveminer@gmail.com>
2019-10-14 11:46:14 -0400

    libs

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject com.velisco/pklbrain "0.1.1-SNAPSHOT"
+(defproject com.velisco/pklbrain "0.2.1-SNAPSHOT"
   :description "PKL Brain"
   :url "https://github.com/miner/pklbrain"
-  :dependencies [[org.clojure/clojure "1.9.0"]
-                 [quil "2.8.0"]])
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [quil "3.1.0"]])
 
